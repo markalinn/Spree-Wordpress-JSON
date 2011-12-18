006a59ef70614c0fc14fe1848feba9802cea8dde
Mark Linn <marklinn@xwcsolutions.com>
2011-12-18 13:40:50 -0800

    Various enhancements.  Still need to work on getting A LOT out of the controllers and into the model,

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,13 +1,31 @@
 class BlogPostsController < Spree::BaseController
   def index
     #@posts = BlogPost.find(:all, :params => {:json => 1, :p => 7464})
-     base_url = WORDPRESS_CONFIG['url'] + "?json=1"
-     url = "#{base_url}&author=#{params[:author]}&page=#{params[:page]}"
-     resp = Net::HTTP.get_response(URI.parse(url))
-     data = resp.body
-     blog_hash = ActiveSupport::JSON.decode(data)
+     @posts =[]
+     @authors = []
+     @categories = []
+     base_url = WORDPRESS_CONFIG['url']
+
+     blog_url = "#{base_url}?json=1&author=#{params[:author]}&page=#{params[:page]}"
+     blog_resp = Net::HTTP.get_response(URI.parse(blog_url))
+     blog_data = blog_resp.body
+     blog_hash = ActiveSupport::JSON.decode(blog_data)
      blog = OpenStruct.new(blog_hash)
      @posts = blog.posts
+
+     authors_url = "#{base_url}?json=get_author_index&page=#{params[:page]}"
+     authors_resp = Net::HTTP.get_response(URI.parse(authors_url))
+     authors_data = authors_resp.body
+     authors_hash = ActiveSupport::JSON.decode(authors_data)
+     blog_authors = OpenStruct.new(authors_hash)
+     @authors = blog_authors.authors
+
+     categories_url = "#{base_url}?json=get_category_index&page=#{params[:page]}"
+     categories_resp = Net::HTTP.get_response(URI.parse(categories_url))
+     categories_data = categories_resp.body
+     categories_hash = ActiveSupport::JSON.decode(categories_data)
+     blog_categories = OpenStruct.new(categories_hash)
+     @categories = blog_categories.categories
 
     respond_to do |format|
       format.html # index.html.erb
